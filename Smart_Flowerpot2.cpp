6b1c1b35d42675d8f480e188504ab2a93f488851
ironmanM85 <corjwl12@gmail.com>
2022-07-13 16:37:04 +0900

    Print LCD

---
+++ Smart_Flowerpot2.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup(){
+	Serial.begin(115200);
+
+	lcd.init();
+	lcd.backlight();
+	lcd.clear();
+
+	lcd.setCursor(2, 0);
+	lcd.print("IoT Project");
+	lcd.setCursor(2, 1);
+	lcd.print("Smart Flowerpot!");
+	delay(1000);
+}
+
+void loop(){
+
+}
