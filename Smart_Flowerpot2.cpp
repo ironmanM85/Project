97bd9024afcf0b2ab04e38af55484dbe8a3cf9a1
ironmanM85 <corjwl12@gmail.com>
2022-07-15 01:54:35 +0900

    Add DHT, Print LCD

--- Smart_Flowerpot2.cpp
+++ Smart_Flowerpot2.cpp
@@ -1,7 +1,12 @@
+#include <DHT.h>
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 
+#define DHTPIN		A0
+#define DHTTYPE		DHT11
+
 LiquidCrystal_I2C lcd(0x27,16,2);
+DHT dht(DHTPIN,DHTTYPE);
 
 void setup(){
 	Serial.begin(115200);
@@ -10,13 +15,29 @@ void setup(){
 	lcd.backlight();
 	lcd.clear();
 
+	pinMode(DHTPIN,INPUT);
+
 	lcd.setCursor(2, 0);
 	lcd.print("IoT Project");
 	lcd.setCursor(0, 1);
 	lcd.print("Smart Flowerpot!");
 	delay(1000);
+
+	lcd.clear();
 }
 
 void loop(){
+	int h = dht.readHumidity();
+	int t = dht.readTemperature();
 
+	Serial.println(String("Humid : ") + h + String(", Temp : ") + t);
+
+	lcd.setCursor(0, 0);
+	lcd.print(String("Humid : ") + h);
+
+	lcd.setCursor(0, 1);
+	lcd.print(String("Temp : ") + t);
+
+
+	delay(1000);
 }
