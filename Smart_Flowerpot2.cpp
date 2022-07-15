66a8e2abf894b727f3e07b85b6a92afc852f157f
ironmanM85 <corjwl12@gmail.com>
2022-07-15 15:07:47 +0900

    Add Temp_Humid SuperLoop Class

--- Smart_Flowerpot2.cpp
+++ Smart_Flowerpot2.cpp
@@ -2,12 +2,43 @@
 #include <Arduino.h>
 #include <Emotion_Farm.h>
 #include <LiquidCrystal_I2C.h>
+#include "util/SuperLoop.h"
 
 #define DHTPIN		A0
 #define DHTTYPE		DHT11
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 DHT dht(DHTPIN,DHTTYPE);
+
+class Temp_Humid : public SuperLoop{
+	int h;
+	int t;
+public:
+	Temp_Humid() : SuperLoop(6000){
+		pinMode(DHTPIN,INPUT);
+	}
+
+	void job() override{
+		readDht();
+	}
+
+	void readDht(){
+		lcd.clear();
+		h = dht.readHumidity();
+		t = dht.readTemperature();
+
+		Serial.println(String("Humid : ") + h + String(" Temp : ") + t);
+
+		lcd.setCursor(1, 0);
+		lcd.write(0);
+		lcd.print(String(" ") + t);
+		lcd.write(1);
+
+		lcd.setCursor(9, 0);
+		lcd.write(2);
+		lcd.print(String(" ") + h + String("%"));
+	}
+};
 
 void setup(){
 	Serial.begin(115200);
@@ -35,25 +66,12 @@ void setup(){
 	lcd.print("IoT Project");
 	lcd.setCursor(0, 1);
 	lcd.print("Smart Flowerpot!");
-	delay(1000);
 
 	lcd.clear();
 }
 
+Temp_Humid temp_humid;
 void loop(){
-	int h = dht.readHumidity();
-	int t = dht.readTemperature();
+	temp_humid.loop();
 
-	Serial.println(String("Humid : ") + h + String(", Temp : ") + t);
-
-	lcd.setCursor(0, 0);
-	lcd.write(2);
-	lcd.print(String("Humid : ") + h);
-
-	lcd.setCursor(0, 1);
-	lcd.write(0);
-	lcd.print(String("Temp : ") + t);
-
-
-	delay(1000);
 }
