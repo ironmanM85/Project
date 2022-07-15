1fd5fead9a2d4b85e8f9086f66c9b7d8b5a45723
ironmanM85 <corjwl12@gmail.com>
2022-07-16 02:04:57 +0900

    Pull Test

--- Smart_Flowerpot2.cpp
+++ Smart_Flowerpot2.cpp
@@ -6,6 +6,12 @@
 #include <LiquidCrystal_I2C.h>
 #include "util/SuperLoop.h"
 
+#define DHTPIN		A0
+#define DHTTYPE		DHT11
+
+LiquidCrystal_I2C lcd(0x27,16,2);
+DHT dht(DHTPIN,DHTTYPE);
+
 class Temp_Humid : public SuperLoop{
 	int h;
 	int t;
