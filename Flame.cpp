874b564b2ccb72b0f67e79d4eba17ff5a87362de
Leehyunjinman <guswk1992@naver.com>
2022-07-19 16:11:28 +0900

    Flame add

--- Flame.cpp
+++ Flame.cpp
@@ -1,9 +1,19 @@
+#if 1
+
 #include <Arduino.h>
+#include <Firmata.h>
+
+int flame = A0;
+int val = 0;
 
 void setup(){
+	pinMode(flame,INPUT);
 	Serial.begin(115200);
 }
 
 void loop(){
-
+	val = analogRead(flame);
+	Serial.println(val);
 }
+
+#endif
