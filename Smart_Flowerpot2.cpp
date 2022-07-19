c74c92d5c0327bf82b5f26641d43ae6fdbbabf62
ironmanM85 <corjwl12@gmail.com>
2022-07-19 18:04:46 +0900

    Soil + Water + DHT

--- Smart_Flowerpot2.cpp
+++ Smart_Flowerpot2.cpp
@@ -1,16 +1,20 @@
-#if 0
+#if 1
 
-#include <DHT.h>
+#include "util/DHT.h"
 #include <Arduino.h>
-#include <Emotion_Farm.h>
-#include <LiquidCrystal_I2C.h>
+#include "util/Emotion_Farm.h"
+#include "util/LiquidCrystal_I2C.h"
 #include "util/SuperLoop.h"
 
 #define DHTPIN		A0
+#define Soil 		A1
+#define PumpA		9
 #define DHTTYPE		DHT11
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 DHT dht(DHTPIN,DHTTYPE);
+
+int LED[3] = { 2, 3, 4 };
 
 class Temp_Humid : public SuperLoop{
 	int h;
@@ -42,14 +46,95 @@ public:
 	}
 };
 
+class WaterPump: public SuperLoop {
+	int prestate = 1;
+public:
+	int state = 0;
+
+	WaterPump() : SuperLoop(3000) {
+		pinMode(PumpA, OUTPUT);
+	}
+
+	void job() override {
+		if(prestate & state){
+			pumpOn();
+			state = 0;
+		}
+		else
+			pumpOff();
+	}
+
+	void pumpOn() {
+		analogWrite(PumpA, 255);
+	}
+
+	void pumpOff() {
+		analogWrite(PumpA, 0);
+	}
+};
+
+WaterPump waterpump;
+
+class Moisture: public SuperLoop {
+	int value = 0;
+public:
+	Moisture() : SuperLoop(6000) {
+		pinMode(Soil, INPUT);
+	}
+
+	void job() override {
+		readSoil();
+	}
+
+	void readSoil() {
+		lcd.clear();
+		value = map(analogRead(Soil), 1023, 200, 0, 100);
+		Serial.print(String("Soil value = ") + analogRead(Soil));
+		Serial.println(String(" Soil map value = ") + value);
+		lcd.setCursor(1, 1);
+
+		lcd.write(4);
+		lcd.print(String(" ") + value + String("%"));
+		lcd.setCursor(9, 1);
+
+		if (value < 25)
+			On();
+		else
+			Off();
+	}
+
+	void On() {
+		waterpump.state = 1;
+
+		for (int i = 0; i < 3; i++) {
+			lcd.write(6);
+			lcd.print(" ");
+		}
+		digitalWrite(LED[0], HIGH);
+		digitalWrite(LED[1], LOW);
+	}
+
+	void Off() {
+		waterpump.state = 0;
+
+		for (int i = 0; i < 3; i++) {
+			lcd.write(5);
+			lcd.print(" ");
+		}
+		digitalWrite(LED[0], LOW);
+		digitalWrite(LED[1], HIGH);
+	}
+};
+
 void setup(){
 	Serial.begin(115200);
+
+	for (int i = 0; i < 3; i++)
+			pinMode(LED[i], OUTPUT);
 
 	lcd.init();
 	lcd.backlight();
 	lcd.clear();
-
-	pinMode(DHTPIN,INPUT);
 
 	lcd.createChar(0, temp);
 	lcd.createChar(1, C);
@@ -67,9 +152,12 @@ void setup(){
 }
 
 Temp_Humid temp_humid;
+Moisture moi;
+
 void loop(){
 	temp_humid.loop();
-
+	moi.loop();
+	waterpump.loop();
 }
 
 #endif
