205cfc93e8f07a43c299e4dcf221298c0e49ce71
ironmanM85 <corjwl12@gmail.com>
2022-07-19 17:32:02 +0900

    1->0

--- Soil_Water.cpp
+++ Soil_Water.cpp
@@ -1,4 +1,4 @@
-#if 1
+#if 0
 
 #include <Arduino.h>
 #include "util/Emotion_Farm.h"
@@ -13,19 +13,19 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 int LED[3] = { 2, 3, 4 };
 
 class WaterPump: public SuperLoop {
-
+	int prestate = 1;
 public:
+	int state = 0;
+
 	WaterPump() : SuperLoop(3000) {
 		pinMode(PumpA, OUTPUT);
 	}
 
 	void job() override {
-		int value = analogRead(Soil);
-
-		Serial.println(String("Soil value = ") + value);
-
-		if (value > 800)
+		if(prestate & state){
 			pumpOn();
+			state = 0;
+		}
 		else
 			pumpOff();
 	}
@@ -39,13 +39,13 @@ public:
 	}
 };
 
+WaterPump waterpump;
+
 class Moisture: public SuperLoop {
-	int value;
+	int value = 0;
 public:
 	Moisture() : SuperLoop(6000) {
 		pinMode(Soil, INPUT);
-		for (int i = 0; i < 3; i++)
-			pinMode(LED[i], OUTPUT);
 	}
 
 	void job() override {
@@ -58,6 +58,7 @@ public:
 		Serial.print(String("Soil value = ") + analogRead(Soil));
 		Serial.println(String(" Soil map value = ") + value);
 		lcd.setCursor(1, 1);
+
 		lcd.write(4);
 		lcd.print(String(" ") + value + String("%"));
 		lcd.setCursor(9, 1);
@@ -69,6 +70,8 @@ public:
 	}
 
 	void On() {
+		waterpump.state = 1;
+
 		for (int i = 0; i < 3; i++) {
 			lcd.write(6);
 			lcd.print(" ");
@@ -78,17 +81,22 @@ public:
 	}
 
 	void Off() {
+		waterpump.state = 0;
+
 		for (int i = 0; i < 3; i++) {
 			lcd.write(5);
 			lcd.print(" ");
 		}
 		digitalWrite(LED[0], LOW);
-		digitalWrite(LED[1], OUTPUT);
+		digitalWrite(LED[1], HIGH);
 	}
 };
 
 void setup() {
 	Serial.begin(115200);
+
+	for (int i = 0; i < 3; i++)
+		pinMode(LED[i], OUTPUT);
 
 	lcd.init();
 	lcd.backlight();
@@ -103,6 +111,7 @@ Moisture moi;
 
 void loop() {
 	moi.loop();
+	waterpump.loop();
 }
 
 #endif
