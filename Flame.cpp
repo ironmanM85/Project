c6330fecd9e5339a09b5c98cb234b0a0e98d1b2d
Leehyunjinman <guswk1992@naver.com>
2022-07-19 17:23:47 +0900

    1 -> 0

--- Flame.cpp
+++ Flame.cpp
@@ -1,13 +1,16 @@
-#if 1
+#if 0
 
 #include <Arduino.h>
 #include <Firmata.h>
 
 int flame = A0;
+int Buzzer = 5;
 int val = 0;
+
 
 void setup(){
 	pinMode(flame,INPUT);
+	pinMode(Buzzer,OUTPUT);
 	Serial.begin(115200);
 }
 
